b13d770a3d0f033a4e5e78ea68f61ce5953aa4a9
Dravin <dravin.hot@gmail.com>
2019-06-01 12:22:16 +0800

    7

--- 7.矩阵键盘/main.c
+++ 7.矩阵键盘/main.c
@@ -1,53 +1,92 @@
-#include "reg52.h"
-#include "intrins.h"
+/**************************************************************************************
+*		              矩阵按键实验												  *
+实现现象：下载程序后数码管显示0，按下矩阵按键上的按键显示对应的数字
+			S1-S4：0-3
+			S5-S8：4-7
+			S9-S12：8-B
+			S13-S16：C-F。
+注意事项：																				  
+***************************************************************************************/
 
+#include "reg52.h"			 //此文件中定义了单片机的一些特殊功能寄存器
+
+typedef unsigned int u16;	  //对数据类型进行声明定义
 typedef unsigned char u8;
-typedef unsigned int u16;
 
 #define GPIO_DIG P0
 #define GPIO_KEY P1
 
-u8 code smgduan[16]={0x3f,0x06,0x5b,0x4f,
-					0x66,0x6d,0x7d,0x07,
-					0x7f,0x6f,0x77,0x7c,
-					0x39,0x5e,0x79,0x71};
-u8 keyValue;
-void delay(u16 i){while(i--);}
 
-//这个函数能修改KeyValue的值0-15
-void keyDown(){
+u8 KeyValue;	//用来存放读取到的键值
+
+
+u8 code smgduan[17]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,
+					0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71};//显示0~F的值
+
+/*******************************************************************************
+* 函 数 名         : delay
+* 函数功能		   : 延时函数，i=1时，大约延时10us
+*******************************************************************************/
+void delay(u16 i)
+{
+	while(i--);	
+}
+
+/*******************************************************************************
+* 函 数 名         : KeyDown
+* 函数功能		   : 检测有按键按下并读取键值
+* 输    入         : 无
+* 输    出         : 无
+*******************************************************************************/
+void KeyDown(void)
+{
 	char a=0;
 	GPIO_KEY=0x0f;
-	if(GPIO_KEY!=0x0f){
-		delay(1000);
-		if(GPIO_KEY!=0x0f){
-			 switch(GPIO_KEY){
-			 	case(0x07) :keyValue=0;break;
-				case(0x0b) :keyValue=1;break;
-				case(0x0d) :keyValue=2;break;
-				case(0x0e) :keyValue=3;break;
-			 }
-			 GPIO_KEY=0xf0;
-			 switch(GPIO_KEY){
-			 	case(0x70) :keyValue=keyValue;break;
-				case(0xb0) :keyValue=keyValue+4;break;
-				case(0xd0) :keyValue=keyValue+8;break;
-				case(0xe0) :keyValue=keyValue+12;break;
-			 }
-			 while((a<50)&&(GPIO_KEY!=0xf0)){
-			 	 delay(1000);
-				 a++;
-			 }
+	if(GPIO_KEY!=0x0f)//读取按键是否按下
+	{
+		delay(1000);//延时10ms进行消抖
+		if(GPIO_KEY!=0x0f)//再次检测键盘是否按下
+		{	
+			//测试列
+			GPIO_KEY=0X0F;
+			switch(GPIO_KEY)
+			{
+				case(0X07):	KeyValue=0;break;
+				case(0X0b):	KeyValue=1;break;
+				case(0X0d): KeyValue=2;break;
+				case(0X0e):	KeyValue=3;break;
+			}
+			//测试行
+			GPIO_KEY=0XF0;
+			switch(GPIO_KEY)
+			{
+				case(0X70):	KeyValue=KeyValue;break;
+				case(0Xb0):	KeyValue=KeyValue+4;break;
+				case(0Xd0): KeyValue=KeyValue+8;break;
+				case(0Xe0):	KeyValue=KeyValue+12;break;
+			}
+			while((a<50)&&(GPIO_KEY!=0xf0))	 //检测按键松手检测
+			{
+				delay(1000);
+				a++;
+			}
 		}
 	}
-}		  
+}
 
-void main(){
 
-	while(1){
-	
-		keyDown();	
-		GPIO_DIG=~smgduan[keyValue];
-	}
-	
+/*******************************************************************************
+* 函 数 名       : main
+* 函数功能		 : 主函数
+* 输    入       : 无
+* 输    出    	 : 无
+*******************************************************************************/
+void main()
+{	
+
+	while(1)
+	{	
+		KeyDown();		   //按键判断函数
+		GPIO_DIG=~smgduan[KeyValue];	  //
+	}		
 }
